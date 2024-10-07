d20982f3c657edb2368d5b5b81a74a259252c6de
Davida <byourself77by@gmail.com>
2024-10-07 14:28:27 +0000

    ADD controller/book

---
+++ controllers/book.js
@@ -0,0 +1,56 @@
+export const addBook = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(201).json("Book Added successfully!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getBooks = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(200).json("Books Retrived!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getBook = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(200).json("Book Retrived!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateBook = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(200).json("Book retrived!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteBooks = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(200).json("Book retrived!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+
