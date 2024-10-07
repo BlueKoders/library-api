75759ac3fc711a5e37c50a741cb1e0b18b2ea210
Davida <byourself77by@gmail.com>
2024-10-07 15:14:14 +0000

    ADD more js files to routes, contollers and model folder

--- controllers/user.js
+++ controllers/user.js
@@ -3,17 +3,15 @@
 //Register 
  export const register = (req, res, next) => {
 try {
-    
         //Respondse to register request
         res.status(201).json("Thanks for registering! We've reserved your space!")
 } catch (error) {
     next(error);
 }
  }
- 
+
  export const login = (req, res, next) => {
 try {
-    
         //Respondse to register request
         res.status(201).json("login successful!")
 } catch (error) {
@@ -23,8 +21,7 @@ try {
 
  export const logout = (req, res, next) => {
 try {
-    
-        //Respondse to register request
+    //Respondse to register request
         res.status(201).json("Logout successful, see you soon!")
 } catch (error) {
     next(error);
