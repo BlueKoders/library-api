78f665252adfe2125048b846ccd541e1d7580e01
Jacob Delanyo Dotsey <dotseydelanyo11@gmail.com>
2024-10-07 16:10:39 +0000

    changed error

--- routes/book.js
+++ routes/book.js
@@ -7,12 +7,7 @@ const bookRouter = Router();
 
 
 // Define routes
-<<<<<<< HEAD
 bookRouter.post('/books', addBook);
-
-=======
-bookRouter.post('/books', addBook)
->>>>>>> a0b040f79d10578ef523b2cf98120a167cf8132e
 bookRouter.get('/books', getBooks);
 bookRouter.get('/books/:id', getBook);
 bookRouter.patch('/books/:id', updateBook);
