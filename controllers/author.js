0a3de25308c5ea75b33db98c98ae1894c833de4b
Davida <byourself77by@gmail.com>
2024-10-08 09:46:17 +0000

    ADD controllers/author

---
+++ controllers/author.js
@@ -0,0 +1,54 @@
+export const addAuthor = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(201).json("Author Added successfully!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const allAuthor = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(201).json("Authors retrieved successfully!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const anAuthor = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(201).json("Author retrieved successfully!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateAuthor = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(201).json("Author updated successfully!");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteAuthor = async (req, res, next) => {
+    try {
+        //Validate user input
+        //write todo to database
+        //Respond to request
+        res.status(201).json("Author deleted successfully!");
+    } catch (error) {
+        next(error);
+    }
+}
