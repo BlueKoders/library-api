44a9ff377fd1203dcf9b0f17e6f362981587921a
Davida <byourself77by@gmail.com>
2024-10-11 13:56:04 +0000

    mm

--- validators/book.js
+++ validators/book.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const addBookValidator = Joi.object({
     title:Joi.string().required().min(2).max(200),
-    summary:Joi.string().required().min(20).max(1000),
+    summary:Joi.string().required().min(2).max(1000),
     author:Joi.string().required(),
     publisher:Joi.string().required(),
     publishDate: Joi.string(),
